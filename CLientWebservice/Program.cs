cda548a82eba54b4bd35e4edfa360cff760a3e0b
Malek Hilali <Malek Hilali>
2017-10-11 23:11:50 +0200

    Ajoutez des fichiers projet.

---
+++ CLientWebservice/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLientWebservice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var client = new LemonWayService.WebServiceSoapClient())
+            {
+                var fabSuite = client.Fibonacci(10);
+                Console.WriteLine(fabSuite);
+            }
+
+            for (;;) { }
+
+        }
+    }
+}
